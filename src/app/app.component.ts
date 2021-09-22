7521151cabc7e9a54c219a1a806e14007f54c194
LostTurtle <khaloodm25@gmail.com>
2021-09-22 09:30:57 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  showPassword = false;
+  togglePasswordView() {
+    this.showPassword = !this.showPassword
+  }
+
+  isHeader = true
+
+  toggleHeaderView () {
+    this.isHeader = !this.isHeader
+  }
+
+  classes () {
+    return {
+      bold: this.isHeader,
+      underline: this.isHeader,
+      header: this.isHeader,
+      subHeader: !this.isHeader,
+      italic: !this.isHeader,
+    }
+  }
+
+
+  todos: {title: string, id: number, completed: boolean}[] = [{
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+    },
+    {
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+    },
+    {
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+    },
+    {
+    id: 4,
+    title: "et porro tempora",
+    completed: true
+    },
+    {
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false
+    },
+  ]
+
+
+  // newTodo: {title: string}[] = [{
+  //   title: ''
+  // }]
+
+  newTodo = ""
+  addNewTodo() {
+    const todo = {
+      title: this.newTodo,
+      id: new Date().getTime(),
+      completed: false
+    }
+    this.todos.push(todo);
+    this.newTodo = '';
+  }
+
+  removeTodo(id: number) {
+    const todoIndex = this.todos.findIndex(todo => todo.id == id)
+    if (todoIndex > -1) {
+      this.todos.splice(todoIndex, 1)
+    }
+  }
+
 }
